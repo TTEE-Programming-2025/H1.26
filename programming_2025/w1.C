423bc5a78e3a8055d16a12ee47087ad29d249525
柳辰翰 <hank200591@gmail.com>
2025-06-05 02:51:47 +0800

    加入 w1.C

---
+++ programming_2025/w1.C
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h> // 用來 getch()
+
+#define PASSWORD "2024"
+
+void clearScreen() {
+    system("cls"); // 清除螢幕（Windows）
+}
+
+int main() {
+    char inputPassword[10];
+    char userInput;
+
+    // 開始畫面
+    printf("***************************************\n");
+    printf("**           Welcome 歡迎!           **\n");
+    printf("**                                   **\n");
+    printf("**           By 柳辰翰               **\n");
+    printf("**                                   **\n");
+    printf("**             ^     ^               **\n");
+    printf("**            (()__(())              **\n");
+    printf("**           //       \\\\              **\n");
+    printf("**          //(  0 0  )\\\\             **\n");
+    printf("**          // (  w  ) \\\\             **\n");
+    printf("**         \\\\ __________//            **\n");
+    printf("**         000000000000000           **\n");
+    printf("**        00000000000000000          **\n");
+    printf("**     11 000000000000000000 11      **\n");
+    printf("**   11    0000000000000000    11    **\n");
+    printf("** 11    0000000000000000000     11  **\n");
+    printf("**           1111   1111             **\n");
+    printf("**           1111   1111             **\n");
+    printf("**           1111   1111             **\n");
+    printf("**           1111   1111       H1.26 **\n");
+    printf("** __________________________________**\n");
+    for (int i = 0; i < 5; i++) {
+        printf("**                                   **\n");
+    }
+
+    getch();
+    return 0;
+}
+
+    
+    
+    
+   
